b79ca931bb1a81891de658bc0edbd2309bba9103
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-04 17:50:19 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,20 @@
+var questao1 = (numero1, numero2) => {
+    let resultado = numero1 * numero2;
+    console.log(resultado);
+  }
+  
+  var questao1 = (numero1, numero2, numero3) =>{
+    let num1 = numero1 = 1;
+    let num2 = numero2 = 2;
+    let num3 = numero3 = 3;
+    console.log(num3);
+  }
+  
+  var questao3 = (number) =>{
+    if (number % 2 == 0){
+      console.log(number + " par");
+    }
+    else {
+      console.log(number + " impar")
+    }
+  }
